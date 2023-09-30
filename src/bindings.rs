8dfd9a034020821f1ac6956f117f694f95f7ba41
zhangyuang <leonin0829@gmail.com>
2023-10-01 01:48:28 +0800

    feat: support object type

--- src/bindings.rs
+++ src/bindings.rs
@@ -34,10 +34,51 @@ extern "C" {
 }
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
+pub struct Parent {
+  pub name: *const ::std::os::raw::c_char,
+  pub age: ::std::os::raw::c_int,
+}
+#[test]
+fn bindgen_test_layout_Parent() {
+  const UNINIT: ::std::mem::MaybeUninit<Parent> = ::std::mem::MaybeUninit::uninit();
+  let ptr = UNINIT.as_ptr();
+  assert_eq!(
+    ::std::mem::size_of::<Parent>(),
+    16usize,
+    concat!("Size of: ", stringify!(Parent))
+  );
+  assert_eq!(
+    ::std::mem::align_of::<Parent>(),
+    8usize,
+    concat!("Alignment of ", stringify!(Parent))
+  );
+  assert_eq!(
+    unsafe { ::std::ptr::addr_of!((*ptr).name) as usize - ptr as usize },
+    0usize,
+    concat!(
+      "Offset of field: ",
+      stringify!(Parent),
+      "::",
+      stringify!(name)
+    )
+  );
+  assert_eq!(
+    unsafe { ::std::ptr::addr_of!((*ptr).age) as usize - ptr as usize },
+    8usize,
+    concat!(
+      "Offset of field: ",
+      stringify!(Parent),
+      "::",
+      stringify!(age)
+    )
+  );
+}
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
 pub struct Person {
   pub name: *const ::std::os::raw::c_char,
   pub age: ::std::os::raw::c_int,
-  pub parent: *mut Person,
+  pub parent: Parent,
 }
 #[test]
 fn bindgen_test_layout_Person() {
@@ -45,7 +86,7 @@ fn bindgen_test_layout_Person() {
   let ptr = UNINIT.as_ptr();
   assert_eq!(
     ::std::mem::size_of::<Person>(),
-    24usize,
+    32usize,
     concat!("Size of: ", stringify!(Person))
   );
   assert_eq!(
