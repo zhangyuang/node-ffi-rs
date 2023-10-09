1e682a85adbe398499ab331064037fd4b5332d92
zhangyuang <leonin0829@gmail.com>
2023-10-09 13:13:54 +0800

    fix: ci

--- build.rs
+++ build.rs
@@ -1,16 +1,26 @@
 extern crate napi_build;
+macro_rules! p {
+    ($($tokens: tt)*) => {
+        println!("cargo:warning={}", format!($($tokens)*))
+    }
+}
 
 fn main() {
-  println!("cargo:rustc-link-search=native=.");
-  let bindings = bindgen::Builder::default()
-    .header("./cpp/sum.h")
-    .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-    .allowlist_function(".*")
-    .raw_line("#[link(name = \"sum\")]")
-    .generate()
-    .expect("Unable to generate bindings");
-  bindings
-    .write_to_file("./src/bindings.rs")
-    .expect("Couldn't write bindings!");
+  let env = std::env::var("env").unwrap_or_else(|_| String::from("production"));
+
+  if env == "development" {
+    println!("cargo:rustc-link-search=native=.");
+    let bindings = bindgen::Builder::default()
+      .header("./cpp/sum.h")
+      .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+      .allowlist_function(".*")
+      .raw_line("#[link(name = \"sum\")]")
+      .generate()
+      .expect("Unable to generate bindings");
+    bindings
+      .write_to_file("./src/bindings.rs")
+      .expect("Couldn't write bindings!");
+  }
+
   napi_build::setup();
 }
