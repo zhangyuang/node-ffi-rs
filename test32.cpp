4b81bbc2cb3060caec44103f7a3afb63c7f12350
zhangyuang <leonin0829@gmail.com>
2024-01-09 12:56:55 +0800

    chore: update

--- test32.cpp
+++ test32.cpp
@@ -1,3 +1,7 @@
 
 
-extern "C" const char *concatenateStrings(const char *cstr) { return cstr; }
+#include <cstdio>
+extern "C" const char *concatenateStrings(const char *cstr) {
+  printf("xx%s", cstr);
+  return cstr;
+}
