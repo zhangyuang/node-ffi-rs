5e2f8b8731942174d089c703c7c565f9bdc86fe4
zhangyuang <leonin0829@gmail.com>
2024-04-04 15:09:41 +0800

    chore: optimize code

--- src/datatype/function.rs
+++ src/datatype/function.rs
@@ -17,7 +17,10 @@ pub unsafe fn get_js_function_call_value(
       let data_type = number_to_basic_data_type(*number);
       let data = match data_type {
         BasicDataType::U8 => RsArgsValue::U8(func_val_ptr as u8),
-        BasicDataType::I32 => RsArgsValue::I32(func_val_ptr as i32),
+        BasicDataType::I32 => {
+          println!("i32{:?}", func_val_ptr);
+          return RsArgsValue::I32(func_val_ptr as i32);
+        }
         BasicDataType::I64 => RsArgsValue::I64(func_val_ptr as i64),
         BasicDataType::U64 => RsArgsValue::U64(func_val_ptr as u64),
         BasicDataType::Boolean => RsArgsValue::Boolean(if func_val_ptr as i32 == 0 {
@@ -36,6 +39,7 @@ pub unsafe fn get_js_function_call_value(
         BasicDataType::Void => RsArgsValue::Void(()),
         // need to be improved
         BasicDataType::Double => {
+          println!("xx{:?}", func_val_ptr);
           panic!("Double type cannot be used as function parameter type so far");
         }
       };
