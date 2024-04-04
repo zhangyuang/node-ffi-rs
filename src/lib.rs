9be4fb03e409fee774f17f79e2694f76617579ad
zhangyuang <leonin0829@gmail.com>
2024-04-04 15:09:41 +0800

    chore: add optionalDependencies

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,13 @@ extern crate napi_derive;
 
 use napi::bindgen_prelude::*;
 
-use napi::{Either, Env, JsNumber, JsUnknown};
+use napi::{Env, JsNumber, JsUnknown};
 
 use libc::c_char;
 use libc::malloc;
 use libffi_sys::{
   ffi_abi_FFI_DEFAULT_ABI, ffi_call, ffi_cif, ffi_prep_cif, ffi_type, ffi_type_pointer,
-  ffi_type_sint32,
+  ffi_type_sint32, ffi_type_void,
 };
 use libloading::{Library, Symbol};
 use std::ffi::c_void;
@@ -19,9 +19,10 @@ use std::ffi::CString;
 pub enum RetType {
   String,
   I32,
+  Void,
 }
 
-pub enum RetTypeStruct {
+pub enum RsArgsValue {
   String(String),
   I32(i32),
 }
@@ -42,7 +43,7 @@ struct FFIParams {
 }
 
 #[napi]
-fn load(env: Env, params: FFIParams) -> Either<String, i32> {
+fn load(params: FFIParams) -> Either3<String, i32, ()> {
   let FFIParams {
     library,
     func_name,
@@ -50,19 +51,18 @@ fn load(env: Env, params: FFIParams) -> Either<String, i32> {
     params_type,
     params_value,
   } = params;
-
   unsafe {
     let lib = Library::new(library).unwrap();
     let func: Symbol<unsafe extern "C" fn()> = lib.get(func_name.as_str().as_bytes()).unwrap();
 
-    let (mut arg_types, arg_values): (Vec<*mut ffi_type>, Vec<RetTypeStruct>) = params_type
+    let (mut arg_types, arg_values): (Vec<*mut ffi_type>, Vec<RsArgsValue>) = params_type
       .iter()
       .zip(params_value.into_iter())
       .map(|(param, value)| match param {
         ParamsType::I32 => {
           let arg_type = &mut ffi_type_sint32 as *mut ffi_type;
           let arg_val: i32 = value.coerce_to_number().unwrap().try_into().unwrap();
-          (arg_type, RetTypeStruct::I32(arg_val))
+          (arg_type, RsArgsValue::I32(arg_val))
         }
         ParamsType::String => {
           let arg_type = &mut ffi_type_pointer as *mut ffi_type;
@@ -73,18 +73,18 @@ fn load(env: Env, params: FFIParams) -> Either<String, i32> {
             .unwrap()
             .try_into()
             .unwrap();
-          (arg_type, RetTypeStruct::String(arg_val))
+          (arg_type, RsArgsValue::String(arg_val))
         }
       })
       .unzip();
     let mut arg_values: Vec<*mut c_void> = arg_values
       .into_iter()
       .map(|val| match val {
-        RetTypeStruct::I32(val) => {
+        RsArgsValue::I32(val) => {
           let c_num = Box::new(val);
           Box::into_raw(c_num) as *mut c_void
         }
-        RetTypeStruct::String(val) => {
+        RsArgsValue::String(val) => {
           let c_string = Box::new(CString::new(val).unwrap());
           Box::into_raw(c_string) as *mut c_void
         }
@@ -94,6 +94,7 @@ fn load(env: Env, params: FFIParams) -> Either<String, i32> {
     let r_type: *mut ffi_type = match ret_type {
       RetType::I32 => &mut ffi_type_sint32 as *mut ffi_type,
       RetType::String => &mut ffi_type_pointer as *mut ffi_type,
+      RetType::Void => &mut ffi_type_void as *mut ffi_type,
     };
 
     let mut cif = ffi_cif {
@@ -126,7 +127,7 @@ fn load(env: Env, params: FFIParams) -> Either<String, i32> {
         );
 
         let result_str = CString::from_raw(result).into_string().unwrap();
-        Either::A(result_str)
+        Either3::A(result_str)
       }
       RetType::I32 => {
         let mut result: i32 = 0;
@@ -136,7 +137,17 @@ fn load(env: Env, params: FFIParams) -> Either<String, i32> {
           &mut result as *mut i32 as *mut c_void,
           arg_values.as_mut_ptr(),
         );
-        Either::B(result)
+        Either3::B(result)
+      }
+      RetType::Void => {
+        let mut result = ();
+        ffi_call(
+          &mut cif,
+          Some(*func),
+          &mut result as *mut () as *mut c_void,
+          arg_values.as_mut_ptr(),
+        );
+        Either3::C(())
       }
     }
   }
