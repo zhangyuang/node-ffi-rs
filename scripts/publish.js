d3444aa84fa896e493904657d235bc3ce341c50e
zhangyuang <leonin0829@gmail.com>
2024-04-04 15:09:41 +0800

    feat: add scope

---
+++ scripts/publish.js
@@ -0,0 +1,15 @@
+const { resolve } =require('path')
+const { cp } = require('shelljs')
+const { promises,  } = require('fs')
+
+const myResolve = dir => resolve(process.cwd(), dir)
+const p = async () => {
+  const folders = await promises.readdir(myResolve('./npm'))
+  for (const item of folders) {
+    if (item!=='.DS_Store') {
+      cp(myResolve('./README.md'), myResolve(`./npm/${item}`))
+    }
+  }
+}
+
+p.then()
