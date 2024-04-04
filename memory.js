043a970365de9653d0cb76d351c604e9be10e599
zhangyuang <leonin0829@gmail.com>
2024-04-04 15:09:41 +0800

    chore: update code by marco

--- memory.js
+++ memory.js
@@ -1,9 +1,9 @@
 const Koa = require('koa');
 const app = new Koa();
-const {unitTest} = require('./test')
+const { unitTest } = require('./test')
 
 app.use(async ctx => {
-  console.log((process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2))
+  console.log('memory:', (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2))
   unitTest()
   ctx.body = 'success'
 });
