8b6cc27215edaf6dca668fe239b7a5957ef7ddf3
zhangyuang <leonin0829@gmail.com>
2024-04-04 15:09:41 +0800

    fix: ci

---
+++ scripts/build.js
@@ -0,0 +1,7 @@
+const { execSync } = require('child_process')
+
+const options = {
+  stdio: 'inherit'
+}
+const target = process.env.target
+execSync(`yarn build:c && napi build --platform --release --js-package-name @yuuang/ffi-rs ${target ? `--target ${target}` : ''}&& node scripts/type.js`, options)
