7c8b0dccc80d2af0487c7c2ca0ba304a83808ff3
zhangyuang <leonin0829@gmail.com>
2024-04-04 15:09:41 +0800

    feat: call tsfn in c other thread

--- src/ffi_macro.rs
+++ src/ffi_macro.rs
@@ -3,30 +3,34 @@ macro_rules! match_args_len {
         match $args_len {
             $(
                 $num => {
-                    let lambda =  |$($arg: *mut c_void),*| {
-                        let uuid = UUID.lock().unwrap();
-                        let uuid = &*uuid;
-                        let uuid = uuid.as_ref().unwrap();
-                        let func_args_type_rs = FUNC_DESC.as_ref().unwrap().get(uuid).unwrap();
+                    fn lambda($($arg: *mut c_void),*) {
+                        let lambda_id = &lambda as *const _ as usize;
+                        unsafe {
+                            let tsfn = TS_FN.as_ref().unwrap().get(&lambda_id).unwrap();
+                            let func_args_type_rs = FUNC_DESC.as_ref().unwrap().get(&lambda_id).unwrap();
+                            let arg_arr = [$($arg),*];
+                            let value: Vec<RsArgsValue> = (0..$num)
+                                .map(|index| {
+                                    let c_param = arg_arr[index as usize];
+                                     let arg_type = func_args_type_rs.get(&index.to_string()).unwrap();
+                                    let param = get_js_function_call_value(arg_type, c_param);
+                                    param
+                                })
+                              .collect();
 
-                        let func_status = STATUS.lock().unwrap();
-                        let func_status = func_status.unwrap();
-                        let tsfn = TS_FN.as_ref().unwrap().get(uuid).unwrap();
-                        let arg_arr = [$($arg),*];
-                        let mut value: Vec<RsArgsValue> = (0..$num)
-                            .map(|index| {
-                                let c_param = arg_arr[index as usize];
-                                 let arg_type = func_args_type_rs.get(&index.to_string()).unwrap();
-                                let param = get_js_function_call_value( arg_type, c_param);
-                                param
-                            })
-                            .collect();
-                    value.push(RsArgsValue::String(uuid.clone()));
-                    value.push(RsArgsValue::Boolean(func_status.clone()));
-                      tsfn.call(value, ThreadsafeFunctionCallMode::NonBlocking);
-                    };
+                            tsfn.call(value, ThreadsafeFunctionCallMode::NonBlocking);
+                        };
+
+                    }
+                    let lambda_id = &lambda as *const _ as usize;
+                    FUNC_DESC
+                      .as_mut()
+                      .unwrap()
+                      .insert(lambda_id, $func_args_type_rs);
+                      TS_FN.as_mut().unwrap().insert(lambda_id, $tsfn);
                     let closure = Box::into_raw(Box::new($closure::new(&lambda)));
                     return std::mem::transmute((*closure).code_ptr());
+
                 }
             )*
             _ => {
