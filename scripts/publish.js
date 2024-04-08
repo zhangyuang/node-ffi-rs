b523dee768f3c16e999fd46a181213bab3998632
zhangyuang <leonin0829@gmail.com>
2024-04-08 11:13:20 +0800

    1.0.56

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,15 +1,30 @@
-const { resolve } =require('path')
+const { resolve } = require('path')
 const { cp } = require('shelljs')
-const { promises,  } = require('fs')
+const { promises } = require('fs')
 
 const myResolve = dir => resolve(process.cwd(), dir)
 const p = async () => {
   const folders = await promises.readdir(myResolve('./npm'))
   for (const item of folders) {
-    if (item!=='.DS_Store') {
+    if (item !== '.DS_Store') {
       cp(myResolve('./README.md'), myResolve(`./npm/${item}`))
     }
   }
+  const pkgJson = require(resolve(process.cwd(), './package.json'))
+  pkgJson['name'] = '@yuuang/ffi-rs'
+  await promises.writeFile(resolve(process.cwd(), './package.json'), JSON.stringify(pkgJson, "", "\n"))
 }
 
-p().then()
+const main = async () => {
+  const pkgJson = require(resolve(process.cwd(), './package.json'))
+  pkgJson['name'] = 'ffi-rs'
+  await promises.writeFile(resolve(process.cwd(), './package.json'), JSON.stringify(pkgJson, "", "\n"))
+}
+
+if (process.env.OPTIONAL) {
+  p().then()
+}
+
+if (process.env.MAIN) {
+  main().then()
+}
