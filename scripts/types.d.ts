66e5b8d6ba651cda38a9d26684249c302d2d456a
zhangyuang <leonin0829@gmail.com>
2024-04-30 16:14:36 +0800

    chore: update code by marco

---
+++ scripts/types.d.ts
@@ -0,0 +1,64 @@
+/* tslint:disable */
+/* eslint-disable */
+
+type DataTypeToType<T extends DataType> =
+  T extends DataType.String ? string :
+  T extends DataType.I32 ? number :
+  T extends DataType.Double ? number :
+  T extends DataType.I32Array ? number[] :
+  T extends DataType.StringArray ? string[] :
+  T extends DataType.DoubleArray ? number[] :
+  T extends DataType.Boolean ? boolean :
+  T extends DataType.Void ? undefined :
+  never
+
+type DataFieldTypeToType<T extends DataFieldType<DataType>> =
+  T extends DataType ? DataTypeToType<T> :
+  T extends ArrayConstructorOptions<infer U> ? DataTypeToType<U> :
+  never;
+
+export function load<T extends DataType, U extends Record<string, DataFieldType<T>>>(params: FfiParams<T> & {
+  retType: U
+}): { [K in keyof U]: DataFieldTypeToType<U[K]> }
+
+export function load<T extends DataType>(params: FfiParams<T> & {
+  retType: T
+}): DataTypeToType<T>
+
+export type ArrayConstructorOptions<T extends DataType> = {
+  type: T
+  length: number
+  ffiTypeTag?: string
+}
+export function arrayConstructor<T extends DataType>(options: ArrayConstructorOptions<T>): ArrayConstructorOptions<T>
+
+export function load<T extends DataType>(params: FfiParams<T> & {
+  retType: ArrayConstructorOptions<T>
+}): DataTypeToType<T>
+
+export type DataFieldType<T extends DataType> = DataType | Record<string, DataType> | ArrayConstructorOptions<T>
+
+
+export const enum DataType {
+  String = 0,
+  I32 = 1,
+  Double = 2,
+  I32Array = 3,
+  StringArray = 4,
+  DoubleArray = 5,
+  Boolean = 6,
+  Void = 7
+}
+export interface FfiParams<T extends DataType> {
+  library: string
+  funcName: string
+  retType: DataFieldType<T>
+  paramsType: Array<DataFieldType<T>>
+  paramsValue: Array<unknown>
+}
+export interface OpenParams {
+  library: string
+  path: string
+}
+export function open(params: OpenParams): void
+export function close(library: string): void
