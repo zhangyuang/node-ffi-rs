f9e8b5d8949912728cc9ecb3550399f37439a80b
zhangyuang <leonin0829@gmail.com>
2024-05-09 15:27:12 +0800

    feat: update publish script support alpha

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,35 +1,46 @@
-const { resolve } = require('path')
-const { cp } = require('shelljs')
-const { promises } = require('fs')
-const { execSync } = require('child_process')
+const { resolve } = require('path');
+const { cp } = require('shelljs');
+const { promises: fsPromises } = require('fs');
+const { execSync } = require('child_process');
 
-const myResolve = dir => resolve(process.cwd(), dir)
-const p = async () => {
-  const folders = await promises.readdir(myResolve('./npm'))
+const myResolve = dir => resolve(process.cwd(), dir);
+
+const isPreReleaseVersion = version => {
+  const preReleaseRegex = /-\w+/;
+  return preReleaseRegex.test(version);
+};
+
+const publishPackage = (path) => {
+  const pkgPath = resolve(path, 'package.json');
+  const pkgJson = require(pkgPath);
+  const { version } = pkgJson;
+  const tag = isPreReleaseVersion(version) ? `--tag alpha` : '';
+  execSync(`npm publish ${tag}`, { cwd: path, stdio: 'inherit' });
+};
+
+const publishSubpackages = async () => {
+  const folders = await fsPromises.readdir(myResolve('./npm'));
   for (const item of folders) {
     if (item !== '.DS_Store') {
-      cp(myResolve('./README.md'), myResolve(`./npm/${item}`))
+      cp(myResolve('./README.md'), myResolve(`./npm/${item}`));
+      publishPackage(myResolve(`./npm/${item}`), item);
     }
   }
-  const pkgJson = require(resolve(process.cwd(), './package.json'))
-  pkgJson['name'] = '@yuuang/ffi-rs'
-  await promises.writeFile(resolve(process.cwd(), './package.json'), JSON.stringify(pkgJson, null, 2))
-}
+};
 
-const main = async () => {
-  const pkgJson = require(resolve(process.cwd(), './package.json'))
-  pkgJson['name'] = 'ffi-rs'
-  await promises.writeFile(resolve(process.cwd(), './package.json'), JSON.stringify(pkgJson, null, 2))
-  execSync('npm publish', { stdio: 'inherit' })
-  pkgJson['name'] = 'node-ffi-rs'
-  await promises.writeFile(resolve(process.cwd(), './package.json'), JSON.stringify(pkgJson, null, 2))
-  execSync('npm publish', { stdio: 'inherit' })
-}
+const publishRoot = async () => {
+  const rootPath = process.cwd();
+  publishPackage(rootPath, 'ffi-rs');
+  publishPackage(rootPath, 'node-ffi-rs');
+};
 
-if (process.env.OPTIONAL) {
-  p().then()
-}
-
-if (process.env.MAIN) {
-  main().then()
-}
+publishSubpackages()
+  .then(async () => {
+    await publishRoot();
+  })
+  .then(() => {
+    console.log('Publish succeeded');
+  })
+  .catch(err => {
+    console.error(`Publish failed: ${err}`);
+  });
