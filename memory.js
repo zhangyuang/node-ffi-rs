faf6be079d2884f3af8f12717da607e8d07ddda3
zhangyuang <leonin0829@gmail.com>
2024-05-12 16:44:41 +0800

    chore: update memory.js

--- memory.js
+++ memory.js
@@ -4,7 +4,7 @@ process.env.MEMORY = 1
 const { unitTest } = require('./test')
 
 app.use(async ctx => {
-  console.log('memory:', (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2))
+  console.log('memory:', (process.memoryUsage().rss / 1024 / 1024).toFixed(2))
   unitTest()
   ctx.body = 'success'
 });
